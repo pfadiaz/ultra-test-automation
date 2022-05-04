b75034f95d502d45910dd431e513fe4291644d05
Pablo Fallas <pablofallas@Pablos-iMac.local>
2022-05-04 00:20:15 -0600

    chores: adding custom commands and test data generators

---
+++ tests/utils/custom-commands.js
@@ -0,0 +1,38 @@
+const addBrowserCustomCommands = function () {
+  browser.addCommand(
+    'clickOn',
+    function () {
+      this.waitForDisplayed();
+      this.click();
+    },
+    true
+  );
+
+  browser.addCommand(
+    'type',
+    function (text) {
+      this.waitForDisplayed();
+      this.click();
+      this.setValue(text);
+    },
+    true
+  );
+
+  // WAIT UTILS
+
+  browser.addCommand('waitUntilPageFullyLoaded', function (url) {
+    this.waitUntil(
+      () => {
+        const pageState = browser.execute(() => {
+          return 'complete' === document.readyState;
+        });
+        const currentUrl = browser.getUrl();
+
+        return true === pageState && currentUrl.includes(url);
+      },
+      { timeoutMsg: `Expected page was not Loaded: ${url}` }
+    );
+  });
+};
+
+module.exports = { addBrowserCustomCommands };
