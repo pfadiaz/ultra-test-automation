0c12dd68e136e5e20f8d10bf66cd331fa8b0664b
Pablo Fallas <pablofallas@Pablos-iMac.local>
2022-05-04 23:08:18 -0600

    chores: adding expect library and prettier from airbnb

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+{
+}
+module.exports = {
+  ...require('prettier-airbnb-config'),
+  arrowParens: 'always',
+  bracketSpacing: true,
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'all',
+  importOrderSeparation: false,
+  printWidth: 120,
+};
